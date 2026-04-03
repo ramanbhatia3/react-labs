6cb861824ac20452bde7efa2af9af018b3ee5948
ramanbhatia3 <bhattiramandeep3105@gmail.com>
2026-04-03 19:28:45 +0530

    class based components

---
+++ 10-class-based-components/src/App.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+const App = () => {
+  return (
+    <div>
+      <ClassCounter />
+    </div>
+  )
+}
+
+class ClassCounter extends Component{
+  state = { count: 0 };
+
+  increment = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  render(){
+    return (
+      <div>
+        <p>Count: {this.state.count}</p>
+        <button onClick={this.increment}>Increment</button>
+      </div>
+    )
+  }
+}
+
+export default App
