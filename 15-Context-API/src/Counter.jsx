19689d760ef0db4048073762a8f09a9942b76f14
ramanbhatia3 <bhattiramandeep3105@gmail.com>
2026-04-26 11:44:17 +0530

    Counter using Context API

---
+++ 15-Context-API/src/Counter.jsx
@@ -0,0 +1,47 @@
+import React, {createContext, useContext, useState} from "react";
+
+const CountContext = createContext()
+
+function CountContextProvider({children}){
+    const [count, setCount] = useState(0)
+
+    return <CountContext.Provider value={{count, setCount}}>
+        {children}
+    </CountContext.Provider>
+}
+
+function Parent(){
+    return (
+        <CountContextProvider>
+            <Increase />
+            <Decrease />
+            <Value />
+        </CountContextProvider>
+    )
+}
+
+function Decrease(){
+    const {setCount} = useContext(CountContext)
+
+    return <button onClick={() => setCount(count => count-1)}>Decrease</button>
+}
+
+function Increase(){
+    const {setCount} = useContext(CountContext)
+
+    return <button onClick={() => setCount(count => count+1)}>Increase</button>
+}
+
+function Value(){
+    const {count} = useContext(CountContext)
+
+    return <p>Count: {count}</p>
+}
+
+const Count = () => {
+    return <div>
+        <Parent />
+    </div>
+}
+
+export default Count;
--- 15-Context-API/src/main.jsx
+++ 15-Context-API/src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 // import App from './App.jsx'
-import App from './Application.jsx'
+import Counter from './Counter.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Counter />
   </StrictMode>,
 )
